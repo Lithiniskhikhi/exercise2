69062362ab360e01a6620b4195565e4c7328fb52
Lithiniskhikhi <lithiniskhikhi9@gmail.com>
2023-07-31 16:04:56 +0200

    done

---
+++ index.js
@@ -0,0 +1,13 @@
+// Import the Person constructor from the person.js module
+const Person = require('./person');
+
+// Create three different objects using the Person constructor
+const person1 = new Person('Rabia', 'Haucha', 'Rabia.Haucha.com');
+const person2 = new Person('Summer', 'Dewet', 'Summer.Dewet.com');
+const person3 = new Person('Lithini', 'Skhikhi', 'Lithini.Skhikhi.com');
+
+// Display the created objects
+console.log('Person 1:', person1);
+console.log('Person 2:', person2);
+console.log('Person 3:', person3);
+
---
+++ person.js
@@ -0,0 +1,9 @@
+// Define the Person constructor
+function Person(firstName, lastName, emailAddress) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.emailAddress = emailAddress;
+  }
+  
+  // Export the Person constructor
+  module.exports = Person;
